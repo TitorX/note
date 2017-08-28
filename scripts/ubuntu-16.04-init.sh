9d7fe6511c988e19d7948410d2112578ee0c1891
titorx <sun.qd.china@outlook.com>
2017-08-29 01:42:16 +0800

    Ubuntu16.04安装脚本

--- scripts/ubuntu-16.04-init.sh
+++ scripts/ubuntu-16.04-init.sh
@@ -19,13 +19,27 @@ EOF
 sudo apt update
 
 # frequently used tools
-sudo apt install tmux htop vim
+sudo apt install tmux htop vim supervisor
 
 # python2 related tools
 sudo apt install python-dev python-pip
+mkdir ~/.pip
+# set pip source
+cat > ~/.pip/pip.conf << EOF
+
+[global]
+index-url = http://pypi.douban.com/simple
+
+[install]
+trusted-host=pypi.douban.com
+
+EOF
+
+sudo pip install --upgrade pip
 sudo pip install virtualenv
 sudo pip install "ipython<6.0"
 sudo pip install jupyter
+sudo pip install shadowsocks
 
 # install zsh&oh-my-zsh
 sudo apt install zsh
